5cff06ec680e89a93790c6cc6c66107a177cd4ea
Ronan McHugh <mchugh.r@gmail.com>
2014-10-09 21:57:10 +0200

    Bibframe::Work module with test.

---
+++ app/models/bibframe/work.rb
@@ -0,0 +1,13 @@
+module Bibframe
+  # All Bibframe specific logic for Works
+  # should live in this module
+  module Work
+    extend ActiveSupport::Concern
+    included do
+      has_metadata(name: 'bfMetadata',
+                   type: Datastreams::Bibframe::WorkMetadata)
+      has_attributes(:title, datastream: 'bfMetadata', multiple: false)
+      has_attributes(:language, datastream: 'bfMetadata', multiple: true)
+    end
+  end
+end
---
+++ spec/models/bibframe/work_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+# Dummy class for testing
+class Stub < ActiveFedora::Base
+  include Bibframe::Work
+end
+
+describe Bibframe::Work do
+  describe 'On inclusion' do
+    before :all do
+      @stub = Stub.new
+    end
+    it 'should add Bibframe datastream' do
+      expect(@stub.datastreams.keys).to include('bfMetadata')
+    end
+
+    it 'should add title method' do
+      expect(@stub.respond_to?(:title)).to eql true
+    end
+
+    it 'should add a language method' do
+      expect(@stub.respond_to?(:language)).to eql true
+    end
+  end
+end
