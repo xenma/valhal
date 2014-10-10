831333491629ffedb36535eae6266542f6b27dc9
Ronan McHugh <mchugh.r@gmail.com>
2014-10-10 17:48:41 +0200

    Work model.

---
+++ app/models/work.rb
@@ -0,0 +1,9 @@
+# This class represents a Work model in
+# Fedora. Only KB specific logic should
+# live in this class. All domain logic
+# e.g. Bibframe, Hydra::Rights etc,
+# should live in separate modules and
+# be mixed in.
+class Work < ActiveFedora::Base
+  include Bibframe::Work
+end
