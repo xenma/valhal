35c0dd4e250650d1846f06cf616452b3edd4fdc7
Ronan McHugh <mchugh.r@gmail.com>
2014-10-15 14:39:50 +0200

    functioning MADS datastream.

--- app/models/datastreams/mads/document.rb
+++ app/models/datastreams/mads/document.rb
@@ -8,7 +8,17 @@ module Datastreams
     class Document < ActiveFedora::OmDatastream
       set_terminology do |t|
         t.root(path: 'mads', xmlns: 'http://www.loc.gov/mads/v2')
+        t.notes(path: 'note')
         t.authority do
+          t.name do
+            t.authority_type(path: { attribute: 'authority' })
+            t.family(path: 'namePart', attributes: { type: 'family' })
+            t.given(path: 'namePart', attributes: { type: 'given' })
+            t.date(path: 'namePart', attributes: { type: 'date' })
+            t.full(path: 'namePart', attributes: { type: :none })
+          end
+        end
+        t.variant do
           t.name do
             t.family(path: 'namePart', attributes: { type: 'family' })
             t.given(path: 'namePart', attributes: { type: 'given' })
@@ -18,8 +28,69 @@ module Datastreams
         end
       end
 
+      # Return a hash of hashes
+      # whereby each hash contains the
+      # components of a name element
+      # ordered by their types.
+      # The first-level hash key is the authority type.
+      # If there is no authority type, we use an index.
+      # e.g. { naf: {family: 'Getz', given: 'Stanley', date: '1927'}}
+      def authorized_personal_names
+        authorized = {}
+        authority.name.nodeset.each_with_index do |node, i|
+          type = node.attr('authority') || i
+          authorized[type.to_sym] = parse_names(node)
+        end
+        authorized
+      end
+
+      def other_personal_names
+        other_names = []
+        variant.name.nodeset.each do |node|
+          other_names << parse_names(node)
+        end
+        other_names
+      end
+
       def self.xml_template
         Nokogiri::XML.parse('<mads:mads version="2.0" xmlns:mads="http://www.loc.gov/mads/v2">')
+      end
+
+      private
+
+      # given a mads:name element,
+      # parse the subelements
+      # and return a hash organised
+      # by element type
+      # @param Nokogiri::XML::Node
+      # @return Hash
+      # e.g. {family: 'Getz', given: 'Stan'}
+      def parse_names(node)
+        h = {}
+        family = fetch_name_type(node, 'family')
+        given = fetch_name_type(node, 'given')
+        date = fetch_name_type(node, 'date')
+        full = fetch_name_type(node)
+        h.merge!(family) unless family.nil?
+        h.merge!(given) unless given.nil?
+        h.merge!(date) unless date.nil?
+        h.merge!(full) unless full.nil?
+        h
+      end
+
+      # Given a node and a name type, find the
+      # nearest namePart with that type
+      def fetch_name_type(node, type = nil)
+        # if we have a type, find that type
+        # otherwise find the namePart with no type
+        if type
+          elem = node.xpath('mads:namePart[@type=$value]', nil, value: type)
+        else
+          type = 'full'
+          elem = node.xpath('mads:namePart[not(@*)]')
+        end
+        return nil unless elem.text.present?
+        { type.to_sym => elem.text }
       end
     end
   end
--- spec/models/datastreams/mads/mads_document_spec.rb
+++ spec/models/datastreams/mads/mads_document_spec.rb
@@ -9,18 +9,45 @@ describe Datastreams::MADS::Document do
     end
 
     describe 'authorized_personal_names' do
-      it 'should parse authorised family names'
-      it 'should parse an authorised given name'
-      it 'should parse an authorised date'
-      it 'should return a hash organised by authority types'
+      it 'returns a hash of authorized names' do
+        expect(@ds.authorized_personal_names).to be_a Hash
+      end
+
+      it 'organises the hash by the authority type' do
+        expect(@ds.authorized_personal_names.keys).to include(:naf)
+      end
+
+      it 'should parse authorised family names' do
+        expect(@ds.authorized_personal_names[:naf].keys).to include(:family)
+      end
+      it 'should parse an authorised given name' do
+        expect(@ds.authorized_personal_names[:naf].keys).to include(:given)
+      end
+      it 'should parse an authorised date' do
+        expect(@ds.authorized_personal_names[:naf].keys).to include(:date)
+      end
+
     end
 
-    describe 'variants' do
-      it 'should return an array of other names'
+    describe 'other_personal_names' do
+      it 'should return an array of other names' do
+        expect(@ds.other_personal_names).to be_an Array
+      end
+
+      it 'should parse the full name when present' do
+        expect(@ds.other_personal_names.first.keys).to include(:full)
+        expect(@ds.other_personal_names.first[:full]).to eql 'Gayetsky, Stanley'
+      end
     end
 
     describe 'notes' do
-      it 'should return a list of notes'
+      it 'should return an array of notes' do
+        expect(@ds.notes).to be_an Array
+      end
+
+      it 'should parse the note elements' do
+        expect(@ds.notes.first).to include('Sauter, E.E. St')
+      end
     end
   end
 end
