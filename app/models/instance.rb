02b0c88a0527cd936c838379993176fbd962e932
jatr <jatr@kb.dk>
2014-10-17 14:23:33 +0200

    Some more missing files and config

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -9,4 +9,32 @@ class Instance < ActiveFedora::Base
   belongs_to :work, property: :instance_of
   has_many :content_files, property: :content_for
   has_metadata :name => 'preservationMetadata', :type => Datastreams::PreservationDatastream
+  has_attributes :preservation_profile, :preservation_state, :preservation_details, :preservation_modify_date,
+                 :preservation_comment, :warc_id, :preservation_bitsafety, :preservation_confidentiality,
+                 datastream: 'preservationMetadata', :multiple => false
+
+  before_validation(:on => :create) do
+    self.preservation_profile = 'Undefined' if preservation_profile.blank?
+    self.preservation_state = Constants::PRESERVATION_STATE_NOT_STARTED.keys.first if preservation_state.blank?
+    self.preservation_details = 'N/A' if preservation_details.blank?
+    set_preservation_modified_time(self)
+  end
+
+  # @return whether any operations can be cascading (e.g. updating administrative or preservation metadata)
+  # For the instances, this is true (since it has the files).
+  def can_perform_cascading?
+    true
+  end
+
+  # Check whether it should be cascading, and also perform the cascading.
+  # @param params The parameter from the controller. Contains the parameter for whether the preservation
+  # should be cascaded.
+  # @param element The element to have stuff cascaded.
+  def cascade_preservation(params, element)
+    if element.can_perform_cascading? && params['preservation']['cascade_preservation'] == Constants::CASCADING_EFFECT_TRUE
+      element.cascading_elements.each do |pib|
+        update_preservation_profile_from_controller(params, pib)
+      end
+    end
+  end
 end
