65438269b987172e5009afc88f05a363f819343c
Ronan McHugh <mchugh.r@gmail.com>
2014-10-20 22:33:20 +0200

    new specs and a little reorganisation.

--- spec/models/bibframe/instance_spec.rb
+++ spec/models/bibframe/instance_spec.rb
@@ -9,49 +9,56 @@ describe Bibframe::Instance do
   before :all do
     @stub = Stub.new
   end
-  it 'should allow us to set a language value' do
-    @stub.language = 'eng'
-    expect(@stub.language).to eql ['eng']
-  end
-  it 'should allow us to set a language authority' do
-    @stub.language_authority = 'I says'
-    expect(@stub.language_authority).to eql ['I says']
-  end
-  it 'should allow us to set an identifier value' do
-    @stub.identifier_value = '12345'
-    expect(@stub.identifier_value).to eql ['12345']
-  end
-  it 'should allow us to set an identifier scheme' do
-    @stub.identifier_scheme = 'aleph'
-    expect(@stub.identifier_scheme).to eql ['aleph']
-  end
-  it 'should allow us to set a production date' do
-    @stub.production_date = '1999'
-    expect(@stub.production_date).to eql '1999'
-  end
-  it 'should allow us to set a production note' do
-    @stub.production_note = 'by giant robots!'
-    expect(@stub.production_note).to eql 'by giant robots!'
-  end
-  it 'should allow us to set a distribution date' do
-    @stub.distribution_date = '2000'
-    expect(@stub.distribution_date).to eql '2000'
-  end
-  it 'should allow us to set a distribution note' do
-    @stub.distribution_note = 'On llamas!'
-    expect(@stub.distribution_note).to eql 'On llamas!'
-  end
-  it 'should allow us to set a publication date' do
-    @stub.publication_date = '2000'
-    expect(@stub.publication_date).to eql '2000'
-  end
-  it 'should allow us to set a publication note' do
-    @stub.publication_note = 'On the bottom of your fridge'
-    expect(@stub.publication_note).to eql 'On the bottom of your fridge'
-  end
-  it 'should allow us to set and get isbn13' do
-    @stub.isbn13 = '9780521169004'
-    expect(@stub.isbn13).to eql '9780521169004'
+
+  describe 'literals' do
+    it 'should allow us to set a language value' do
+      @stub.language = 'eng'
+      expect(@stub.language).to eql ['eng']
+    end
+    it 'should allow us to set a language authority' do
+      @stub.language_authority = 'I says'
+      expect(@stub.language_authority).to eql ['I says']
+    end
+    it 'should allow us to set an identifier value' do
+      @stub.identifier_value = '12345'
+      expect(@stub.identifier_value).to eql ['12345']
+    end
+    it 'should allow us to set an identifier scheme' do
+      @stub.identifier_scheme = 'aleph'
+      expect(@stub.identifier_scheme).to eql ['aleph']
+    end
+    it 'should allow us to set a production date' do
+      @stub.production_date = '1999'
+      expect(@stub.production_date).to eql '1999'
+    end
+    it 'should allow us to set a production note' do
+      @stub.production_note = 'by giant robots!'
+      expect(@stub.production_note).to eql 'by giant robots!'
+    end
+    it 'should allow us to set a distribution date' do
+      @stub.distribution_date = '2000'
+      expect(@stub.distribution_date).to eql '2000'
+    end
+    it 'should allow us to set a distribution note' do
+      @stub.distribution_note = 'On llamas!'
+      expect(@stub.distribution_note).to eql 'On llamas!'
+    end
+    it 'should allow us to set a publication date' do
+      @stub.publication_date = '2000'
+      expect(@stub.publication_date).to eql '2000'
+    end
+    it 'should allow us to set a publication note' do
+      @stub.publication_note = 'On the bottom of your fridge'
+      expect(@stub.publication_note).to eql 'On the bottom of your fridge'
+    end
+    it 'should allow us to set and get isbn13' do
+      @stub.isbn13 = '9780521169004'
+      expect(@stub.isbn13).to eql '9780521169004'
+    end
   end
 
+  describe 'relations' do
+    it 'is an instance of a work'
+    it 'can have parts which are Works'
+  end
 end
--- spec/models/bibframe/work_spec.rb
+++ spec/models/bibframe/work_spec.rb
@@ -10,25 +10,38 @@ describe Bibframe::Work do
     before :all do
       @stub = Stub.new
     end
-    it 'should add Bibframe datastream' do
-      expect(@stub.datastreams.keys).to include('bfMetadata')
+    describe 'datastreams' do
+      it 'should add Bibframe datastream' do
+        expect(@stub.datastreams.keys).to include('bfMetadata')
+      end
     end
 
-    it 'should add title method' do
-      expect(@stub.respond_to?(:title)).to eql true
+    describe 'literals' do
+      it 'should add title method' do
+        expect(@stub.respond_to?(:title)).to eql true
+      end
+
+      it 'should add a subtitle method' do
+        expect(@stub.respond_to?(:subtitle)).to eql true
+      end
+
+      it 'should add a language method' do
+        expect(@stub.respond_to?(:language)).to eql true
+      end
+
+      it 'should be able to save a title' do
+        @stub.title = 'War and Peace'
+        expect(@stub.title).to eql 'War and Peace'
+      end
     end
 
-    it 'should add a subtitle method' do
-      expect(@stub.respond_to?(:subtitle)).to eql true
+    describe 'relations' do
+      it 'has many Instances'
+      it 'can be related to other works'
+      it 'can be preceded by other works'
+      it 'can be followed by other works'
+      it 'can be part of an Instance'
     end
 
-    it 'should add a language method' do
-      expect(@stub.respond_to?(:language)).to eql true
-    end
-
-    it 'should be able to save a title' do
-      @stub.title = 'War and Peace'
-      expect(@stub.title).to eql 'War and Peace'
-    end
   end
 end
