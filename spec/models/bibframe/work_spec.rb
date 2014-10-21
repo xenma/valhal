d64445de45f2e54f8a380552416ff23a2b136d8a
Ronan McHugh <mchugh.r@gmail.com>
2014-10-21 22:56:42 +0200

    update tests to reflect API changes.

--- spec/models/bibframe/work_spec.rb
+++ spec/models/bibframe/work_spec.rb
@@ -6,42 +6,42 @@ class Stub < ActiveFedora::Base
 end
 
 describe Bibframe::Work do
-  describe 'On inclusion' do
-    before :all do
-      @stub = Stub.new
+  before :all do
+    @stub = Stub.new
+  end
+  describe 'Datastreams:' do
+    it 'should add Bibframe datastream' do
+      expect(@stub.datastreams.keys).to include('bfMetadata')
     end
-    describe 'datastreams' do
-      it 'should add Bibframe datastream' do
-        expect(@stub.datastreams.keys).to include('bfMetadata')
-      end
-    end
+  end
 
-    describe 'literals' do
-      it 'should add title method' do
-        expect(@stub.respond_to?(:title)).to eql true
-      end
-
-      it 'should add a subtitle method' do
-        expect(@stub.respond_to?(:subtitle)).to eql true
-      end
-
-      it 'should add a language method' do
-        expect(@stub.respond_to?(:language)).to eql true
-      end
-
-      it 'should be able to save a title' do
-        @stub.title = 'War and Peace'
-        expect(@stub.title).to eql 'War and Peace'
-      end
+  describe 'Literals:' do
+    it 'should be possible to add a title' do
+      @stub.add_title(value: 'The Importance of Being Earnest')
+      expect(@stub.title_values).to include('The Importance of Being Earnest')
     end
 
-    describe 'relations' do
-      it 'has many Instances'
-      it 'can be related to other works'
-      it 'can be preceded by other works'
-      it 'can be followed by other works'
-      it 'can be part of an Instance'
+    it 'should be possible to add a language with a part' do
+      @stub.add_language(value: 'French', part: 'undefined')
+      expect(@stub.language_values).to include 'French'
     end
 
+    it 'should be possible to add a language without a part' do
+      @stub.add_language(value: 'Gaeilge')
+      expect(@stub.language_values).to include 'Gaeilge'
+    end
+
+    it 'should be possible to add an identifier' do
+      @stub.add_identifier(scheme: 'kb', value: 'hoojabooja')
+      expect(@stub.identifiers[:kb]).to eql 'hoojabooja'
+    end
+  end
+
+  describe 'Relations:' do
+    it 'has many Instances'
+    it 'can be related to other works'
+    it 'can be preceded by other works'
+    it 'can be followed by other works'
+    it 'can be part of an Instance'
   end
 end
