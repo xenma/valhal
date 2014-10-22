56c6a0d4c03a5c32a23881724ef391dccdc2a16a
Ronan McHugh <mchugh.r@gmail.com>
2014-10-22 12:30:18 +0200

    Use Instance class instead of Stub (fixes test for some reason)

--- spec/models/bibframe/instance_spec.rb
+++ spec/models/bibframe/instance_spec.rb
@@ -1,13 +1,8 @@
 require 'spec_helper'
 
-# Dummy class for testing
-class Stub < ActiveFedora::Base
-  include Bibframe::Instance
-end
-
 describe Bibframe::Instance do
   before :all do
-    @stub = Stub.new
+    @instance = Instance.new
   end
 
   describe 'on include' do
@@ -25,43 +20,43 @@ describe Bibframe::Instance do
 
   describe 'literals' do
     it 'should allow us to set and get notes' do
-      @stub.note = ['a note']
-      expect(@stub.note).to eql ['a note']
+      @instance.note = ['a note']
+      expect(@instance.note).to eql ['a note']
     end
 
     it 'should allow us to set and get a copyright date' do
-      @stub.copyright_date = '19-02-2012'
-      expect(@stub.copyright_date).to eql '19-02-2012'
+      @instance.copyright_date = '19-02-2012'
+      expect(@instance.copyright_date).to eql '19-02-2012'
     end
 
     it 'should allow us to set and get isbn13' do
-      @stub.isbn13 = '9780521169004'
-      expect(@stub.isbn13).to eql '9780521169004'
+      @instance.isbn13 = '9780521169004'
+      expect(@instance.isbn13).to eql '9780521169004'
     end
 
     it 'should allow us to set and get a mode of issuance' do
-      @stub.mode_of_issuance = 'printed'
-      expect(@stub.mode_of_issuance).to eql 'printed'
+      @instance.mode_of_issuance = 'printed'
+      expect(@instance.mode_of_issuance).to eql 'printed'
     end
 
     it 'should allow us to set and get a title statement' do
-      @stub.title_statement = 'Published in Rome'
-      expect(@stub.title_statement).to eql 'Published in Rome'
+      @instance.title_statement = 'Published in Rome'
+      expect(@instance.title_statement).to eql 'Published in Rome'
     end
 
     it 'should allow us to set and get an extent' do
-      @stub.extent = 'incomplete'
-      expect(@stub.extent).to eql 'incomplete'
+      @instance.extent = 'incomplete'
+      expect(@instance.extent).to eql 'incomplete'
     end
 
     it 'should allow us to set and get dimensions' do
-      @stub.dimensions = '36cm'
-      expect(@stub.dimensions).to eql '36cm'
+      @instance.dimensions = '36cm'
+      expect(@instance.dimensions).to eql '36cm'
     end
 
     it 'should allow us to set and get a uuid' do
-      @stub.uuid = 'veryrandomuuid'
-      expect(@stub.uuid).to eql 'veryrandomuuid'
+      @instance.uuid = 'veryrandomuuid'
+      expect(@instance.uuid).to eql 'veryrandomuuid'
     end
 
   end
