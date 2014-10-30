3b64f6cfc4072b11ddb6b6c0d3bcd6fdc3216d81
Ronan McHugh <mchugh.r@gmail.com>
2014-10-30 16:41:04 +0100

    person view files

--- app/controllers/authority/people_controller.rb
+++ app/controllers/authority/people_controller.rb
@@ -1,5 +1,11 @@
 module Authority
   # Get most functionality from BasesController
   class PeopleController < Authority::BasesController
+
+    private
+
+    def set_klazz
+      @klazz = Authority::Person
+    end
   end
 end
