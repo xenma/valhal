ea03a7d115432120960071142635d9afec8260ab
Ronan McHugh <mchugh.r@gmail.com>
2014-10-30 22:22:46 +0100

    functional update and changes to view.

--- app/controllers/authority/people_controller.rb
+++ app/controllers/authority/people_controller.rb
@@ -1,11 +1,18 @@
 module Authority
   # Get most functionality from BasesController
   class PeopleController < Authority::BasesController
+    # overwrite setter and whitelist
 
     private
 
     def set_klazz
       @klazz = Authority::Person
+    end
+
+    def authority_base_params
+      params.require(:authority_person).permit(authorized_personal_name: [
+        [:scheme, :full, :family, :given, :date]
+      ])
     end
   end
 end
