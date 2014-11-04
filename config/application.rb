5fcc816182f8be5243a501aaeebe97457487abf1
Ronan McHugh <mchugh.r@gmail.com>
2014-11-04 13:15:55 +0100

    setup default locale as Danish. Some translation data.

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,6 @@ module Hel
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :da
   end
 end
