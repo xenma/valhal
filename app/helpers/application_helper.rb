b66d99ea6e43d00fafd1ea281ffc8af61d8b31b3
Ronan McHugh <mchugh.r@gmail.com>
2014-11-04 13:16:55 +0100

    add language dropdown to bibliographic metadata.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,14 @@ module ApplicationHelper
     list.nil? ? [] : list.elements.map(&:name)
   end
 
+  # Given a list name, return a list of arrays
+  # suitable for dropdowns, whereby the display
+  # string value is translated via i18n
+  def get_translated_list(list_name)
+    list = get_controlled_vocab(list_name)
+    list.map{ |e| [I18n.t("#{list_name}.#{e}".to_sym), e]}
+  end
+
   # Renders a title type ahead field
   def render_title_typeahead_field
     results = Work.get_title_typeahead_objs
