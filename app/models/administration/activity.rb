c5c9e17a97528d47e85142b430eeca238ff6e280
Sigfrid Lundberg <siggelundberg@gmail.com>
2014-11-04 16:09:28 +0100

    no comments

--- app/models/administration/activity.rb
+++ app/models/administration/activity.rb
@@ -1,3 +1,11 @@
-class Activity < ActiveFedora::Base
+# -*- encoding : UTF-8 -*-
+module Administration
+  class Activity < ActiveFedora::Base
+    include Hydra::AccessControls::Permissions
 
+
+    has_metadata name: 'adminDatastream', type: Datastreams::AdminDatastream
+    has_metadata name: 'preservationDatastream', type: Datastreams::PreservationDatastream
+
+  end
 end
