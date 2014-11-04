d169c9df93620e673ebfd92d05e0edf615c05e27
Ronan McHugh <mchugh.r@gmail.com>
2014-11-04 12:26:11 +0100

    method clean-up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,22 @@
+# View helpers that are shared between different models
 module ApplicationHelper
+  # Gets the list of a controlled list
+  # @param name of a controlled list
+  def get_controlled_vocab(list_name)
+    list = Administration::ControlledList.with(:name, list_name)
+    list.nil? ? [] : list.elements.map(&:name)
+  end
 
-    #Gets the list of a controlled list
-    #@param name of a controlled list
-    def get_controlled_vocab(controlled_vocab_name)
-     vocabulary = Administration::ControlledList.with(:name, controlled_vocab_name)
-     vocabulary.nil? ? [] : vocabulary.elements.map { |e| e.name }
-    end
+  # Renders a title type ahead field
+  def render_title_typeahead_field
+    results = Work.get_title_typeahead_objs
+    select_tag 'work[titles][][value]', options_for_select(results.map { |result| collect_title(result['title_tesim'],result['id']) }.flatten(1)),
+               { include_blank: true, class: 'combobox form-control input-large', data_function: 'title-selected' }
+  end
 
-    #Renders a title type ahead field
-    def render_title_typeahead_field
-      results = Work.get_title_typeahead_objs
-      select_tag "work[titles][][value]", options_for_select(results.collect {|result| collect_title(result['title_tesim'],result['id'])}.flatten(1)),
-                 {include_blank: true, class: 'combobox form-control input-large', data_function: 'title-selected'}
-    end
+  private
 
-    private
-
-
-    def collect_title(titles,id)
-      titles.collect {|title| [title,id]}
-    end
+  def collect_title(titles,id)
+    titles.collect {|title| [title,id]}
+  end
 end
