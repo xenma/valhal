ea4fd5a067d8443fe2fc7d322f0bf29fcce862a8
Ronan McHugh <mchugh.r@gmail.com>
2014-11-05 23:21:20 +0100

    add edtf validation for trykforlaeg.

--- app/models/trykforlaeg.rb
+++ app/models/trykforlaeg.rb
@@ -1,4 +1,8 @@
 # Pdfs sent to printers
 class Trykforlaeg < Instance
+  validates :published_date, presence: true
+  validates_each :published_date do |record, attr, val|
+    record.errors.add(attr, 'must be valid EDTF.') if EDTF.parse(val).nil?
+  end
   validates :isbn13, presence: true, isbn_format: { with: :isbn13 }
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :trykforlaeg do
-    published_date '19-02-2014'
+    published_date '2001-02-03'
     isbn13 '9780521169004'
   end
 end
