d1e558959080e2d16e320e6a2138015422645e4e
Ronan McHugh <mchugh.r@gmail.com>
2014-11-05 21:10:28 +0100

    fix bug to ensure js works on all instance types. little code cleanup.

--- app/assets/javascripts/instance.js
+++ app/assets/javascripts/instance.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-    evaluateEmbargoFields();
-    $embargo_check = $('#instance_embargo');
-    $embargo_check.click(evaluateEmbargoFields);
+   evaluateEmbargoFields();
+   $('[data-toggle="embargo_detailed"]').click(evaluateEmbargoFields);
 
 });
 
 //enable the detailed embargo fields if there is an embargo
 function evaluateEmbargoFields() {
-    $checkbox = $('#instance_embargo');
+    $checkbox = $('[data-toggle="embargo_detailed"]');
     $detailed = $('[data-hook="embargo_detailed"]');
     if ($checkbox.is(':checked')) {
         $detailed.attr('disabled', false);
