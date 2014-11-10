12be1c7c86f7746829a6455a5b071e40453cc556
Sigfrid Lundberg <siggelundberg@gmail.com>
2014-11-10 15:43:59 +0100

    the machinery to get a danmarc2 record encoded as xserver oaimarc is in place

--- app/services/http_service.rb
+++ app/services/http_service.rb
@@ -1,21 +1,19 @@
 # -*- encoding : utf-8 -*-
 require "net/http"
-require "httparty"
 
 class HttpService
   def do_post(uri_string, params)
 
-    logger.debug "Base URl = #{uri_string}"
-    logger.debug "Params = #{params.to_s}"
-    response = false
     begin
-      url = URI.parse(uri_string)
-      logger.debug "http start"
-      response = HTTParty.post(uri_string, :body => params)
+      uri       = URI.parse(uri_string)
+      uri.query = URI.encode_www_form(params)
+      res       = Net::HTTP.get_response(uri)
     rescue Exception  => e
-      logger.error "do_post failed #{uri_string}: #{e.message}"
-      logger.error e.backtrace.join("\n")
+      puts "request failed #{uri_string}: #{e.message}"
+      puts e.backtrace.join("\n")
     end
-    response ? response.body : response
+    # perhaps one should test res.is_a?(Net::HTTPSuccess)
+    # rather than just catching the exception.
+    res.body
   end
 end
--- spec/services/http_service_spec.rb
+++ spec/services/http_service_spec.rb
@@ -9,17 +9,16 @@ describe HttpService do
 
   describe '#get_xml_with_http?' do
     it 'should return a string containing XML from a HTTP request to Aleph' do
-      pending "Does not work externally, e.g. on Travis."
-      stub_request(:post, "http://aleph-00.kb.dk/X").
-         with(:body => {"base"=>"kgl01", "library"=>"kgl01", "op"=>"find", "request"=>"bar=130020101343"},
-              :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'}).
-         to_return(:status => 200, :body => "", :headers => {})
-
       http_service = HttpService.new
-      xml_response = http_service.do_post('http://aleph-00.kb.dk/X', params = {"op" => "find",
-                                                                               "base" => "kgl01",
-                                                                               "library" => "kgl01",
-                                                                               "request" => "bar=130020101343"})
+      xml_response = http_service.do_post(
+                                          'http://rex.kb.dk/X', 
+                                          params = {
+                                            "op"      => "find",
+                                            "base"    => "kgl01",
+                                            "code"    => "",
+                                            "request" => "isbn=9788711396322"
+                                          }
+                                          )
       puts xml_response
     end
   end
