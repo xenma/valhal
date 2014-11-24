333ac23674685bb691f529e61053b4ca0115bae9
David Grove Jørgensen <davidgrove73@gmail.com>
2014-11-24 13:07:11 +0100

    fixed test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,7 +55,7 @@ RSpec.configure do |config|
   config.before :all do
     ActiveRecord::Base.subclasses.each(&:delete_all)
     ActiveFedora::Base.subclasses.each(&:delete_all)
-    a = Administration::Activity.create("activity"=>"Trygforlæg", "collection"=>"billed", "availability"=>"0", "embargo"=>"0", "access_condition"=>"efter aftale", "preservation_profile"=>"storage", "copyright"=>"Attribution-ShareAlike CC BY-SA", "permissions"=>{"instance"=>{"group"=>{"discover"=>["Chronos-Alle"], "read"=>["Chronos-NSA"], "edit"=>["Chronos-Pligtaflevering"]}}, "file"=>{"group"=>{"discover"=>["Chronos-NSA"], "read"=>["Chronos-Pligtaflevering"], "edit"=>["Chronos-Admin"]}}})
+    a = Administration::Activity.create("activity"=>"Trygforlæg", "collection"=>"billed", "availability"=>"0", "embargo"=>"0", "access_condition"=>"efter aftale", "preservation_profile"=>"storage", "copyright"=>"Attribution-ShareAlike CC BY-SA", "permissions"=>{"instance"=>{"group"=>{"discover"=>["Chronos-Alle"], "read"=>["Chronos-NSA"], "edit"=>["Chronos-Pligtaflevering","Chronos-Admin"]}}, "file"=>{"group"=>{"discover"=>["Chronos-NSA"], "read"=>["Chronos-Pligtaflevering"], "edit"=>["Chronos-Admin"]}}})
     @default_activity_id = a.id
   end
 
