fdca4e87c3286b050bfe5e4bdf3daf99e7d59c59
David Grove Jørgensen <davidgrove73@gmail.com>
2014-12-17 12:53:55 +0100

    sorted author select list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,7 +39,8 @@ module ApplicationHelper
     #Renders a list of Agents for a typeahead field
     def get_agent_list
       results = Authority::Agent.get_typeahead_objs
-      results.nil? ? [] : results.collect{|result| [result['display_value_ssm'].first,result['id']]}
+      agents = results.nil? ? [] : results.collect{|result| [result['display_value_ssm'].first,result['id']]}
+      agents.sort {|a,b| a.first.downcase <=> b.first.downcase }
     end
 
     private
