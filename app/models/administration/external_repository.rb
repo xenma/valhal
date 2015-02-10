4ee719beb61b7946742839291b24b1baea4bc0a3
David Grove Jørgensen <davidgrove73@gmail.com>
2015-02-10 16:02:28 +0100

    added external repository mvc

---
+++ app/models/administration/external_repository.rb
@@ -0,0 +1,24 @@
+module Administration
+  class ExternalRepository < OhmModelWrapper
+    attribute :name
+    attribute :type
+    attribute :url
+    attribute :activity
+    attribute :sync_method
+    attribute :sync_status
+    attribute :sync_date
+    list :sync_message, Administration::SyncMessage
+    unique :name
+
+    def clear_sync_messages
+      self.sync_message.each do |msg|
+        msg.delete
+      end
+    end
+
+    def add_sync_message(text)
+      msg = Administration::SyncMessage.create(msg: text)
+      self.sync_message.push(msg)
+    end
+  end
+end
