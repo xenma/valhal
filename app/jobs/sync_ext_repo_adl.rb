a5b8b57c5b2affc890d803d053c66b0b267ce0c5
David Grove Jørgensen <davidgrove73@gmail.com>
2015-02-12 16:30:03 +0100

    added git sync

--- app/jobs/sync_ext_repo_adl.rb
+++ app/jobs/sync_ext_repo_adl.rb
@@ -5,11 +5,45 @@ class SyncExtRepoADL
 
   def self.perform(repo_id)
     repo = Administration::ExternalRepository[repo_id]
+    repo.clear_sync_messages
 
-    repo.sync_status = 'FAILED'
+    if repo.sync_status == 'NEW'
+      success = SyncExtRepoADL.clone(repo)
+    else
+      success = SyncExtRepoADL.update(repo)
+    end
+
+    if (success)
+      repo.sync_status = 'SUCCESS'
+      repo.add_sync_message('Git update success')
+
+
+
+
+    else
+      repo.add_sync_message('Git update failed')
+      repo.sync_status = 'FAILED'
+    end
+
+
+
+
     repo.sync_date = DateTime.now.to_s
-
-    repo.add_sync_message('ADL Sync not implemented yet')
     repo.save
   end
+
+
+  private
+
+  def self.clone(repo)
+    cmd = "git clone #{repo.url} /tmp/adl_data; cd /tmp/adl_data; git fetch; git checkout #{repo.branch}"
+    system( cmd )
+  end
+
+  def self.update(repo)
+    cmd = "cd /tmp/adl_data;git checkout #{repo.branch};git pull #{repo.url} /tmp/adl_data"
+    system( cmd )
+  end
+
+
 end
