9f3a9f0e677b95f4c146bdd95889c5a6f01babd5
David Grove Jørgensen <davidgrove73@gmail.com>
2015-02-19 10:22:45 +0100

    added logging to Resque

--- app/models/content_file.rb
+++ app/models/content_file.rb
@@ -11,6 +11,9 @@ class ContentFile < ActiveFedora::Base
 
   belongs_to :instance, property: :content_for
 
+  # Adds a content datastream to the object as an external managed file in fedore
+  #
+  # @param path external url to the firl
 
   ### custom validations
   ## run through the list of validators in self.validators
@@ -56,6 +59,28 @@ class ContentFile < ActiveFedora::Base
     self.file_uuid = UUID.new.generate
 
     datastreams['content'] = ds
+  end
+
+
+  # This function checks if the content of an external mannaged file
+  # has changed, and updates the tech metadata 
+  def update_tech_metadata_for_external_file
+    if self.datastreams['content'].controlGroup == 'E'
+      path = self.datastreams['content'].dsLocation
+      if path.start_with? 'file://'
+        path.slice! 'file://'
+        file_object = File.new(path)
+        new_checksum = generate_checksum(file_object)
+        logger.debug("#{path} checksums #{self.checksum} #{new_checksum}")
+        if (new_checksum != self.checksum)
+          set_file_timestamps(file_object)
+          self.checksum = new_checksum
+          self.size = file_object.size.to_s
+          return true
+        end
+      end
+    end
+    false
   end
 
 
@@ -128,6 +153,15 @@ class ContentFile < ActiveFedora::Base
 
   def can_perform_cascading?
     false
+  end
+
+  def self.find_by_original_filename(fname)
+    result = ActiveFedora::SolrService.query('original_filename_tesim:"'+fname+'"')
+    if result.size > 0
+      ContentFile.find(result[0]['id'])
+    else
+      nil
+    end
   end
 
   private
