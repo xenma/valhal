61ffb8eed9dd8dadc046fe6f601d9c8259442f61
David Grove Jørgensen <davidgrove73@gmail.com>
2015-02-23 16:21:26 +0100

    added subjects to work view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -110,6 +110,6 @@ class WorksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def work_params
-    params[:work].permit(titles: [[:value, :subtitle, :lang, :type]], creators: [[:id, :type]],note:[])
+    params[:work].permit(titles: [[:value, :subtitle, :lang, :type]], creators: [[:id, :type]], subjects: [[:id]], note:[])
   end
 end
