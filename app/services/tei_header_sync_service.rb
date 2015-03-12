1296c876aa7c6ae9a5d38dfefe188f0c63d4bb3f
Sigfrid Lundberg <siggelundberg@gmail.com>
2015-03-12 16:23:18 +0100

    checking in the header rb code

--- app/services/tei_header_sync_service.rb
+++ app/services/tei_header_sync_service.rb
@@ -1,13 +1,13 @@
 # Responsible for keeping the teiHeader in sync with the hydra chronos metadata
 class TeiHeaderSyncService
-  attr_accessor :file,:sheet,:params
-  def initialize(file, sheet, params)
+  attr_accessor :sheet
+
+  def initialize(sheet)
     @xslt = Nokogiri::XSLT(File.read(sheet))
-    @aleph_marc = marc
   end
 
-  def update_header(@file,@params)
-    doc = Nokogiri::XML.parse(@file) { |config| config.strict }
+  def update_header(file,params)
+    doc = Nokogiri::XML.parse(file) { |config| config.strict }
     @xslt.transform(doc, Nokogiri::XSLT.quote_params(params))
   end
 
--- spec/services/tei_header_sync_service_spec.rb
+++ spec/services/tei_header_sync_service_spec.rb
@@ -3,21 +3,16 @@ require 'spec_helper'
 describe  TeiHeaderSyncService do
 
   before do
-    rec = @service.get_record(set[:set_num],set[:num_entries])
-    @converter = ConversionService.new(rec)
+    @file = File.new("#{Rails.root}/spec/fixtures/holb06valid.xml")
+    xsl = File.new("#{Rails.root}/app/services/xslt/tei_header_sed.xsl")
+    @sync_service = TeiHeaderSyncService.new(xsl)
   end
 
  describe 'update_header' do
     before :all do 
       @instance = Instance.create
+      @work     = Work.create
     end
-    it 'is wellformed XML' do
-      xsd = Nokogiri::XML::Schema(open('http://www.loc.gov/standards/mods/v3/mods-3-5.xsd').read)
-      doc = @converter.to_mods("")
-      puts @converter.to_marc21("")
-      puts doc.to_xml
-      errors = xsd.validate(doc) { |config| config.strict }
-      expect(errors).to eql []
-    end
+   
   end
 end
